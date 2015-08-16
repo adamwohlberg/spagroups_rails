8ed54044daf9dee50ff527f2870609e36f4ac1b8
adamzuckerberg <adam@adamzuckerberg.com>
2015-08-15 18:58:46 -0700

    Choose calendar date or default to this month

--- app/controllers/group_trip_calendar_controller.rb
+++ app/controllers/group_trip_calendar_controller.rb
@@ -2,7 +2,7 @@ class GroupTripCalendarController < ApplicationController
 	def index
 		@reservations = Reservation.all
 		@reservations_by_date = @reservations.group_by(&:arrival_date)
-		@date = Date.today
+		@date = params[:date] ? Date.parse(params[:date]) : Date.today
 	end
 
 	def show
