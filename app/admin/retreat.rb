8fee9a30bd8fac304bc65a0a4eb32bb863bd4647
adamzuckerberg <adam@adamzuckerberg.com>
2015-08-18 15:48:15 -0700

    Complete seed file

--- app/admin/retreat.rb
+++ app/admin/retreat.rb
@@ -1,5 +1,15 @@
 ActiveAdmin.register Retreat do
 
+  index do
+    column :id
+    column :spa_id
+    column :name
+    column :arrival_date
+    column :registered
+    column :confirmed
+    column :group_rates_at
+    actions
+  end
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -13,6 +23,4 @@ ActiveAdmin.register Retreat do
   #   permitted << :other if resource.something?
   #   permitted
   # end
-
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,46 @@
-GuestNight.create!([
-  {reservation_id: 1, guests: 10, date: "2015-08-17"},
-  {reservation_id: 1, guests: 10, date: "2015-08-18"},
-  {reservation_id: 1, guests: 10, date: "2015-08-19"},
-  {reservation_id: 2, guests: 10, date: "2015-08-17"},
-  {reservation_id: 2, guests: 10, date: "2015-08-18"},
-  {reservation_id: 2, guests: 10, date: "2015-08-19"},
-  {reservation_id: 1, guests: 10, date: "2015-08-17"},
-  {reservation_id: 1, guests: 10, date: "2015-08-18"},
-  {reservation_id: 1, guests: 10, date: "2015-08-19"},
-  {reservation_id: 2, guests: 10, date: "2015-08-17"},
-  {reservation_id: 2, guests: 10, date: "2015-08-18"},
-  {reservation_id: 2, guests: 10, date: "2015-08-19"},
-  {reservation_id: 1, guests: 10, date: "2015-08-17"},
-  {reservation_id: 1, guests: 10, date: "2015-08-18"},
-  {reservation_id: 1, guests: 10, date: "2015-08-19"},
-  {reservation_id: 2, guests: 10, date: "2015-08-17"},
-  {reservation_id: 2, guests: 10, date: "2015-08-18"},
-  {reservation_id: 2, guests: 10, date: "2015-08-19"}
-])
-Reservation.create!([
-  {user_id: nil, spa_id: 1, arrival_date: "2015-08-17", first_name: "Adam", email: "adam@foo.com", status: "pending", guests: 10, nights: 3},
-  {user_id: nil, spa_id: 1, arrival_date: "2015-08-17", first_name: "Bob", email: "bob@foo.com", status: "pending", guests: 10, nights: 3}
-])
-Retreat.create!([
-  {arrival_date: "2016-10-01", spa_id: 2, group_leader_id: nil, name: "Old ladies YO!", registered: 12, confirmed: nil, group_rates_at: 8, comp_stay_at: 12, facebook_url: nil, image: nil, facebook_image: nil, description: nil, paypal: nil},
-  {arrival_date: "2015-10-10", spa_id: 1, group_leader_id: nil, name: "Cool name 3", registered: 12, confirmed: nil, group_rates_at: 8, comp_stay_at: 12, facebook_url: nil, image: nil, facebook_image: nil, description: nil, paypal: nil},
-  {arrival_date: "2015-06-08", spa_id: 3, group_leader_id: nil, name: "Cool name 5", registered: 12, confirmed: nil, group_rates_at: 8, comp_stay_at: 12, facebook_url: nil, image: nil, facebook_image: nil, description: nil, paypal: nil},
-  {arrival_date: "2015-10-08", spa_id: 6, group_leader_id: nil, name: "Happy Retreat", registered: 12, confirmed: nil, group_rates_at: 8, comp_stay_at: 12, facebook_url: nil, image: nil, facebook_image: nil, description: nil, paypal: nil},
-  {arrival_date: "2015-05-11", spa_id: 1, group_leader_id: nil, name: "Cool name 1", registered: 12, confirmed: nil, group_rates_at: 8, comp_stay_at: 12, facebook_url: nil, image: nil, facebook_image: nil, description: nil, paypal: nil},
-  {arrival_date: "2015-02-11", spa_id: 3, group_leader_id: nil, name: "Cool name 5", registered: 12, confirmed: nil, group_rates_at: 8, comp_stay_at: 12, facebook_url: nil, image: nil, facebook_image: nil, description: nil, paypal: nil}
-])
-Spa.create!([
-  {name: "Crayon Ranch", city: "Tucson", state: "AZ"},
-  {name: "Crayon Ranch", city: "Lenox", state: "MA"},
-  {name: "Ranch of the Door", city: "Tecate", state: "MX"},
-  {name: "Orange Mountain", city: "St. George", state: "UT"},
-  {name: "Mirabel Resort and Spa", city: "Tucson", state: "AZ"},
-  {name: "The Redwoods at Ojai", city: "Ojai", state: "CA"},
-  {name: "Lake Awesome Resort & Spa", city: "Lake Awesome", state: "TX"},
-  {name: "Trypalu Yoga", city: "Lenox", state: "MA"}
-])
+include Faker
+
+if Rails.env.development?
+
+  # Populate current month
+  80.times do
+  Reservation.create!([
+    {user_id: nil, 
+      spa_id: rand(1..8), 
+      arrival_date: rand(2.months.ago..2.weeks.from_now), 
+      first_name: Faker::Name.first_name, 
+      email: Faker::Internet.email, 
+      status: "pending", 
+      guests: rand(1..5), 
+      nights: rand(2..7)}
+  ])
+  end
+
+  500.times do
+  Reservation.create!([
+    {user_id: nil, 
+      spa_id: rand(1..8), 
+      arrival_date: rand(2.weeks.from_now..3.years.from_now), 
+      first_name: Faker::Name.first_name, 
+      email: Faker::Internet.email, 
+      status: "pending", 
+      guests: rand(1..6), 
+      nights: rand(2..7)}
+  ])
+  end
+
+  Retreat.create!([
+    {arrival_date: "2016-10-01", spa_id: 2, group_leader_id: nil, name: "Old ladies YO!", registered: 12, confirmed: nil, group_rates_at: 8, comp_stay_at: 12, facebook_url: nil, image: nil, facebook_image: nil, description: nil, paypal: nil}
+  ])
+
+  Spa.create!([
+    {name: "Crayon Ranch", city: "Tucson", state: "AZ"},
+    {name: "Crayon Ranch", city: "Lenox", state: "MA"},
+    {name: "Ranch of the Door", city: "Tecate", state: "MX"},
+    {name: "Orange Mountain", city: "St. George", state: "UT"},
+    {name: "Mirabel Resort", city: "Tucson", state: "AZ"},
+    {name: "The Redwoods at Ojai", city: "Ojai", state: "CA"},
+    {name: "Lake Awesome Spa", city: "Lake Awesome", state: "TX"},
+    {name: "Trypalu Yoga", city: "Lenox", state: "MA"}
+  ])
+end
